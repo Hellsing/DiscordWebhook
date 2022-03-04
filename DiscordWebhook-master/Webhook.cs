93063b9dd1bbfccf87af0a038b6880146ae01efc
Pronner <pronnerdev@gmail.com>
2022-03-04 18:52:28 +0400

    Add files via upload

---
+++ DiscordWebhook-master/Webhook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DiscordWebhook
+{
+    class Webhook
+    {
+
+        public static void sendDiscordWebhook(string URL, string escapedjson)
+        {
+            var wr = WebRequest.Create(URL);
+            wr.ContentType = "application/json";
+            wr.Method = "POST";
+            using (var sw = new StreamWriter(wr.GetRequestStream()))
+                sw.Write(escapedjson);
+            wr.GetResponse();
+        }
+
+    }
+}
